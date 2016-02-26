1523f73013d769baf09dc82a66ab46fe27093cb6
Ismaël <Ismal@192.168.1.63>
2016-02-26 15:04:23 +0100

    Spring security + jsf + context + web.xml

---
+++ gestion_magasin/src/fr/mgs/toolbox/Hasher.java
@@ -0,0 +1,19 @@
+package fr.mgs.toolbox;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class Hasher implements PasswordEncoder {
+
+	@Override
+	public String encode(CharSequence arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean matches(CharSequence arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
