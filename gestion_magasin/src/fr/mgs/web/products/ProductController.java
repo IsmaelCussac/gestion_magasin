cfaf1d6667af21256f677880fbd622fb11cd5ba4
clairegerard <clairegerard88@orange.fr>
2016-02-29 09:23:37 +0100

    Push poue regler conflit

--- gestion_magasin/src/fr/mgs/web/products/ProductController.java
+++ gestion_magasin/src/fr/mgs/web/products/ProductController.java
@@ -1,16 +1,13 @@
 package fr.mgs.web.products;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.primefaces.component.log.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 import fr.mgs.business.product.CategoryManager;
 import fr.mgs.business.product.ProductManager;
