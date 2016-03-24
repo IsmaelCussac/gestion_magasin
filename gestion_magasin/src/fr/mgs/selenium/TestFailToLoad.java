711a709028dc704a35587a6894080690edcc3409
MarianaAndujar <mariana.andujarf@gmail.com>
2016-03-24 01:17:46 +0100

    Test Selenium !

---
+++ gestion_magasin/src/fr/mgs/selenium/TestFailToLoad.java
@@ -0,0 +1,87 @@
+package fr.mgs.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestFailToLoad {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://addons.mozilla.org/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testFailToLoad() throws Exception {
+    driver.get(baseUrl + "/en-US/firefox/addon/selenium-expert-selenium-ide/");
+    driver.findElement(By.id("main-wrapper")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("azjeka");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("azeadfq");
+    driver.findElement(By.name("valide_connect")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("mariana");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("azer");
+    driver.findElement(By.name("valide_connect")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("malzk");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("mariana");
+    driver.findElement(By.name("valide_connect")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
